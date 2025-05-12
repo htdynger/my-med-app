3fbd586e5eedf054ab636ab60b69873d6e5d6c9f
htdynger <tipo2008utuber@gmail.com>
2025-05-12 19:13:00 +0500

    error

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Start from './components/Start/Start'
 import SignUp from './components/SignUp/SignUp'
 import Login from './components/Login/Login'
 import Account from './components/Account/Account'
+// import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation';  
+import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation'
 
 
 function App() {
@@ -24,7 +26,7 @@ function App() {
           <Route path='/signup' element={<SignUp />}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/account' element={<Account />}/>
-          
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
       </BrowserRouter>
     </>
