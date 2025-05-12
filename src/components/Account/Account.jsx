65bc885067c337d96d3f975c4dc6c8909464b718
htdynger <tipo2008utuber@gmail.com>
2025-05-12 18:34:03 +0500

    add + fix account page

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,31 +1,44 @@
-import './style.css';
-import { useRef, useEffect } from 'react';
+import './Account.css';
+import { useRef, useState } from 'react';
+
 const Account = () => {
+    const containerRef = useRef(null);
+    const containerRegisterMobileRef = useRef(null);
+    const containerLoginMobileRef = useRef(null);
 
-    const containerRef = useRef(null)
-    
+    const isSmallScreen = window.innerWidth <= 768;
 
-    useEffect(()=>{
-        const container = containerRef.current
-    })
-    
+    const [isLoginMenu, setIsLoginMenu] = useState(true);
+
     const handleRegisterClick = () => {
-        container.classList.add('active')
-    }
+        containerRef.current.classList.add('active');
+    };
 
     const handleLoginClick = () => {
-        container.classList.remove('active')
-    }
+        containerRef.current.classList.remove('active');
+    };
 
+    const handleMobileLoginClick = () => {
+        containerLoginMobileRef.current.classList.remove('unSelectMobile');
+        containerRegisterMobileRef.current.classList.add('unSelectMobile');
+        setIsLoginMenu(true);
+    };
+
+    const handleMobileRegisterClick = () => {
+        containerLoginMobileRef.current.classList.add('unSelectMobile');
+        containerRegisterMobileRef.current.classList.remove('unSelectMobile');
+        setIsLoginMenu(false);
+    };
 
     return (
-            <div className='form-background'>
-                <div ref={containerRef} className="containerAccount mt-3" id="container">
+        <div className='form-background'>
+            {!isSmallScreen ? (
+                <div ref={containerRef} className="containerAccount my-3" id="container">
+                    <div className="background-blur"></div>
+
                     <div className="form-container sign-up">
                         <form>
-
-                            <h1>Create Account</h1>
-
+                            <h1 className='text text-center mb-3'>Create Account</h1>
 
                             <label htmlFor="role">Role</label>
                             <select id="role" className="form-select">
@@ -46,7 +59,7 @@ const Account = () => {
                             <label className='text text-start' htmlFor="password">Password</label>
                             <input id='password' type="password" placeholder="" />
 
-
+                            <br />
 
                             <button className='btn btn-primary' type="submit">Sign Up</button>
                         </form>
@@ -54,35 +67,110 @@ const Account = () => {
 
                     <div className="form-container sign-in">
                         <form>
-                            <h1>Sign In</h1>
-                            <span>or use your email password</span>
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
+                            <h1 className='text text-center mb-3'>Sign In</h1>
+                            
+                            <label htmlFor='email' className='text text-start'> Email </label>
+                            <input id='email' type="email" placeholder="" />
+
+                            <label htmlFor='password' className='text text-start'> Password </label>
+                            <input id='password' type="password" placeholder="Password" />
+
                             <a href="#">Forget Your Password?</a>
 
                             <button className='btn btn-primary' type="submit">Sign In</button>
                         </form>
                     </div>
 
-
                     <div className="toggle-container">
                         <div className="toggle">
                             <div className="toggle-panel toggle-left">
                                 <h1>Welcome Back!</h1>
                                 <p>Enter your personal details to use all of site features</p>
-                                <button onClick={handleLoginClick} className="hidden btn btn-primary" id="login">Sign In</button>
+                                <button
+                                    onClick={handleLoginClick}
+                                    className="hidden btn btn-primary"
+                                    id="login"
+                                >
+                                    Sign In
+                                </button>
                             </div>
                             <div className="toggle-panel toggle-right">
                                 <h1>Hello, Friend!</h1>
                                 <p>
                                     Register with your personal details to use all of site features
                                 </p>
-                                <button onClick={handleRegisterClick} className="hidden btn btn-primary" id="register">Sign Up</button>
+                                <button
+                                    onClick={handleRegisterClick}
+                                    className="hidden btn btn-primary"
+                                    id="register"
+                                >
+                                    Sign Up
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div className='containerAccountMobile mt-5'>
+                    <div className='row text-center'>
+                        <div
+                            onClick={handleMobileLoginClick}
+                            ref={containerLoginMobileRef}
+                            className='col-6'
+                        >
+                            <button className='btn fw-bold'>Sign In</button>
+                        </div>
+                        <div
+                            onClick={handleMobileRegisterClick}
+                            ref={containerRegisterMobileRef}
+                            className='col-6 unSelectMobile'
+                        >
+                            <button className='btn fw-bold'>Sign Up</button>
+                        </div>
+                    </div>
+
+                    {isLoginMenu ? (
+                        <form className='row px-4 my-5'>
+                            <h1 className='my-4 text-center'>Login</h1>
+
+                            <label htmlFor='email'>Email</label>
+                            <input id='email' className='my-2' type="email" placeholder="" />
+
+                            <label htmlFor='password'>Password</label>
+                            <input id='password' className='my-2' type="password" placeholder="" />
+
+                            <button className='btn my-5 btn-primary' type="submit">Sign In</button>
+                        </form>
+                    ) : (
+                        <form className='row px-4 my-5'>
+                            <h1 className='my-4 text-center'>Create Account</h1>
+
+                            <label className='text text-start' htmlFor="role">Role</label>
+                            <select id="role" className="form-select">
+                                <option value="specialist">Специалист</option>
+                                <option value="admin">Администрация</option>
+                                <option value="client">Клиент</option>
+                            </select>
+
+                            <label className='text text-start' htmlFor="name">Name</label>
+                            <input id='name' type="text" placeholder="" />
+
+                            <label className='text text-start' htmlFor="phone">Phone</label>
+                            <input id='phone' type="tel" placeholder="" />
+
+                            <label className='text text-start' htmlFor="email">Email</label>
+                            <input id='email' type="email" placeholder="" />
+
+                            <label className='text text-start' htmlFor="password">Password</label>
+                            <input id='password' type="password" placeholder="" />
+
+
+                            <button className='btn my-5 btn-primary' type="submit">Sign In</button>
+                        </form>
+                    )}
+                </div>
+            )}
+        </div>
     );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
     const navigate = useNavigate()
     // const [burger, setBurger] = useState(false)
-    const burger = window.innerWidth <= 768;
+    const isSmallScreen = window.innerWidth <= 768;
 
     const [navIsVisible, setNavIsVisible] = useState(false)
     const handleBurgerClick = () => {
@@ -16,14 +16,14 @@ const Navbar = () => {
     return (
 
         <>
-            <nav>
+            <nav style={{"boxShadow": "0px 5px 5px -5px rgba(34, 60, 80, 0.6)"}}>
                 
-                {!burger ? <div style={{"borderBottom": "2px solid #000"}} className="row mt-4 align-items-center text-center">
+                {!isSmallScreen ? <div style={{"borderBottom": "2px solid #000"}} className="row pb-4 mt-4 align-items-center text-center">
                         {/* <div className="col-md-2"><img src={logoURL} className="img img-fluid"></img>  </div> */}
                         <div className="col-md-2"> <p className=" fw-bold text-primary"> Stay Health </p> </div> 
 
 
-                        <div className=" col-md-1">
+                        <div className=" col-md-2">
                             <button onClick={()=>navigate('home')} className="btn btn-default">Home</button>
                         </div>
                         <div className=" col-md-2">
@@ -32,20 +32,22 @@ const Navbar = () => {
                         <div className=" col-md-2">
                             <button onClick={()=>navigate('health')} className="btn btn-default">Health Blog</button>
                         </div>
-                        <div className=" col-md-1">
+                        <div className=" col-md-2">
                             <button onClick={()=>navigate('reviews')} className="btn btn-default">Reviews</button>
                         </div>
                         <div className=" col-md-2">
-                            <button onClick={()=>navigate('signup')} className="btn btn-primary">Sign Up</button>
+                            <button onClick={()=>navigate('account')} className="btn btn-primary"> Account </button>
                         </div>
-                        <div className="col-md-2">
-                            <button onClick={()=>navigate('login')} className="btn btn-primary">Login</button>
-                        </div>
+
+
+                        {/* <div className="col-md-2">
+                            <button onClick={()=>navigate('account')} className="btn btn-primary">Sign In</button>
+                        </div> */}
                     </div> 
                 
                 : 
 
-                <div style={{"borderBottom": "2px solid #000"}} className="row align-items-center text-center mt-3">
+                <div style={{"borderBottom": "2px solid #000"}} className="row pb-4 align-items-center text-center mt-3">
                     <div className="col-9"> <p className="h2 fw-bold text-primary"> Stay Health </p> </div> 
 
                     
@@ -55,24 +57,22 @@ const Navbar = () => {
                     </div>
 
                     <div className={navIsVisible ? '' : 'd-none'}> 
-                        <div className="col-12  col-md-auto">
+                        <div className="col-12 my-1 col-md-auto">
                             <button onClick={()=>navigate('home')} className="btn btn-default">Home</button>
                         </div>
-                        <div className="col-12  col-md-auto">
+                        <div className="col-12 my-1 col-md-auto">
                             <button onClick={()=>navigate('appointments')} className="btn btn-default">Appointments</button>
                         </div>
-                        <div className="col-12  col-md-auto">
+                        <div className="col-12 my-1 col-md-auto">
                             <button onClick={()=>navigate('health')} className="btn btn-default">Health Blog</button>
                         </div>
-                        <div className="col-12  col-md-auto">
+                        <div className="col-12 my-1 col-md-auto">
                             <button onClick={()=>navigate('reviews')} className="btn btn-default">Reviews</button>
                         </div>
-                        <div className="col-12  col-md-auto">
-                            <button onClick={()=>navigate('signup')} className="btn btn-primary">Sign Up</button>
+                        <div className="col-12 my-1 col-md-auto">
+                            <button onClick={()=>navigate('account')} className="btn btn-primary"> Account </button>
                         </div>
-                        <div className="col-12  col-md-auto">
-                            <button onClick={()=>navigate('login')} className="btn btn-primary">Login</button>
-                        </div>
+                        
                     </div>
                 </div>
                 
